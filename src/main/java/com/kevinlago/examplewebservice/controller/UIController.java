abda8854c07dcd15e7d74703f63a951a9e2a76c8
Kevin Lago <kevinthelago@gmail.com>
2023-05-22 19:22:56 -0400

    Simple Unauthorized Screen

--- src/main/java/com/kevinlago/examplewebservice/controller/UIController.java
+++ src/main/java/com/kevinlago/examplewebservice/controller/UIController.java
@@ -1,12 +1,33 @@
 package com.kevinlago.examplewebservice.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UIController {
     @GetMapping
     public String getUI() {
         return "index.html";
+    }
+
+    @GetMapping("/error")
+    public String getErrorPage(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer code = Integer.valueOf(status.toString());
+
+            if (code == HttpStatus.NOT_FOUND.value()) {
+                return "404.html";
+            } else if (code == HttpStatus.UNAUTHORIZED.value()) {
+                return "401.html";
+            }
+        }
+
+        return "unmapped error";
     }
 }
